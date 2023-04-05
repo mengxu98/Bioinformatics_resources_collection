21a953a1eebf86446de760d43a841cb21341d155
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-04-05 16:34:57 +0800

    Update serch_code_databases.py

--- serch_code_databases.py
+++ serch_code_databases.py
@@ -6,35 +6,69 @@ from bs4 import BeautifulSoup
 def search_code(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    # content = response.text
-    codeAvailability = soup.find(
+    content = response.text
+
+    code_availability = soup.find(
         "div", {"id": "code-availability-content"}).text
     # Regular expression matching link
     pattern = re.compile(
         r'\((http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)\)')
-    links = re.findall(pattern, codeAvailability)
+    links = re.findall(pattern, code_availability)
 
-    
+    # Traverse all links to check for keywords
     if len(links) > 1:
         for link in links:
             link = link.strip('()')
+            if "github.io" in link:
+                print(f"This link: {link} maybe not a code repository......")
+            else:
+                language = check_link_class(link)
     else:
         for link in links:
             link = link.strip('()')
-    languages = requests.get(link)
-    languages = BeautifulSoup(languages, "html.parser")
+            if "github.io" in link:
+                print(f"This link: {link} maybe not a code repository......")
+            else:
+                language = check_link_class(link)
+            code_infor = [language, link]
 
-    code_links = ["github", "figshare", "zenodo"]
-    for code in code_links:
+    return code_infor
 
-        # Convert the text and the word you want to find to lowercase, and use the split () method to split the text into a word list
-        link = code.lower().split()
-        code = code.lower()
 
-        # Traverse all links to check for keywords
-        for link in links:
-            if code in link:
-                print("Found a", code, "link:", link)
+def check_link_class(link):
+    if "github" in link:
+        print("Found a Github repository, link:", link)
+        code_infor = github_filter(link)
+    elif "figshare" in link:
+        print("Found a figshare repository, link:", link)
+        github_filter(link)
+    elif "zenodo" in link:
+        print("Found a zenodo repository, link:", link)
+        github_filter(link)
+    else:
+        print("No repository information......")
+    return code_infor
+
+
+def github_filter(link):
+    # Extract the owner username and name of the warehouse
+    if ".git" in link:
+        link = link.replace(".git", "")
+    owner, repo = link.split("/")[-2:]
+    newLink = f"https://api.github.com/repos/{owner}/{repo}/languages"
+    # Issue a GET request to obtain the programming language type of the warehouse
+    languages = requests.get(newLink)
+    # Check if the response status code is 200 (indicating success)
+    if languages.status_code == 200:
+        # Obtain response JSON data
+        data = languages.json()
+        # Traverse the programming language type and its corresponding number of lines, and print it out
+        for language, lines in data.items():
+            print(f"Deatiled information: {language}, total {lines} lines")
+            code_infor = [language, link]
+            return code_infor
+    else:
+        print(f"Request failed with status code {languages.status_code}")
 
 
 def search_databases(url):
