02122bfa0c6d1b092b928160183f54d5e634e76a
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-04-07 13:34:56 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+# URL
+# The URL of paper obtain from: https://www.semanticscholar.org/
+url_paper = "https://www.semanticscholar.org/paper/a9c31400520977e0edd854c92edc4efd4f44798f"
+# url_paper = "https://api.semanticscholar.org/v1/paper/a9c31400520977e0edd854c92edc4efd4f44798f" # test
+code_language = ""
+url_code = ""
+data_database = ""
+url_data = ""
