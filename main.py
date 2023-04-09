7a11fb5fae1f2fdfb5050f6053940ff9acce470c
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-04-09 19:34:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from extract_paper_information import extract_paper_infor
 
 # URL
 # The URL of paper obtain from: https://www.semanticscholar.org/
-url_paper = "https://www.semanticscholar.org/paper/a9c31400520977e0edd854c92edc4efd4f44798f"
-# url_paper = "https://api.semanticscholar.org/v1/paper/a9c31400520977e0edd854c92edc4efd4f44798f" # test
+url_paper = "https://www.semanticscholar.org/paper/Single-cell-RNA-sequencing-reveals-distinct-tumor-Bischoff-Trinks/66d79bafce806798b72b4333854b8761073221cc"
+# url_paper = "https://api.semanticscholar.org/v1/paper/66d79bafce806798b72b4333854b8761073221cc" # test
 code_language = ""
 url_code = ""
 data_database = ""
 url_data = ""
 
-extract_paper_infor(url_paper, code_language,
-                    url_code, data_database, url_data, file="test.md")
+extract_paper_infor(url_paper,
+                    code_language,
+                    url_code,
+                    data_database,
+                    url_data,
+                    file="test.md") # "README.md"
