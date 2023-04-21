869477e16c0ad0c4ccc639fe9810abf64f228df5
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-04-21 20:18:04 +0800

    Update serch_code_databases.py

--- serch_code_databases.py
+++ serch_code_databases.py
@@ -4,8 +4,13 @@ from bs4 import BeautifulSoup
 
 
 def format_code(code_language, url_code, url):
+    """
+    Search for code information: code_language, url_code and url
+    """
+
     if not url_code and not code_language:
         code_infors = search_code(url)
+
         if code_infors:
             if len(code_infors) == 1:
                 code_language = code_infors[0][0]
@@ -28,13 +33,16 @@ def format_code(code_language, url_code, url):
             code_language = "Unknown"
             shields_color_code = check_color(code_language)
             codes = code_info(code_language, url_code, shields_color_code)
+
     elif url_code and not code_language:
         code_language = "Unknown"
         shields_color_code = check_color(code_language)
         codes = code_info(code_language, url_code, shields_color_code)
+
     elif not url_code and code_language:
         shields_color_code = check_color(code_language)
         codes = code_info(code_language, url_code, shields_color_code)
+
     else:
         shields_color_code = check_color(code_language)
         codes = code_info(code_language, url_code, shields_color_code)
@@ -42,6 +50,15 @@ def format_code(code_language, url_code, url):
 
 
 def format_data(data_language, url_data, url):
+    """Format data based on the given parameters.
+     Args:
+        data_language (str): The language of the data.
+        url_data (str): The URL of the data.
+        url (str): The URL of the website.
+     Returns:
+        datas (str): The formatted data.
+    """
+
     if not url_data and not data_language:
         data_infors = search_databases(url)
         if data_infors:
@@ -62,18 +79,25 @@ def format_data(data_language, url_data, url):
                     datas = datas + data
 
         else:
+            # If no data is found
             url_data = ""
             data_language = "Unknown"
             shields_color_data = check_color(data_language)
             datas = data_info(data_language, url_data, shields_color_data)
+
     elif url_data and not data_language:
+        # If URL data is provided but no data language
         data_language = "Unknown"
         shields_color_data = check_color(data_language)
         datas = data_info(data_language, url_data, shields_color_data)
+
     elif not url_data and data_language:
+        # If data language is provided but no URL data
         shields_color_data = check_color(data_language)
         datas = data_info(data_language, url_data, shields_color_data)
+        
     else:
+        # If both data language and URL data are provided
         shields_color_data = check_color(data_language)
         datas = data_info(data_language, url_data, shields_color_data)
     return datas
@@ -116,6 +140,12 @@ def search_code(url):
 
 
 def check_code_link_class(code_link):
+    """
+    Check if the code link contains "github", "figshare" or "zenodo"
+    Note: the code_link should be in the format of "https://github.com/user
+    Note: now figshare and zenodo not supported yet, only return 'Unknown' information
+    """
+
     if "github" in code_link:
         print("Found a Github repository, code_link:", code_link)
         code_infor = github_filter(code_link)
@@ -130,68 +160,68 @@ def check_code_link_class(code_link):
         return code_infor
     else:
         print("No repository information......")
-        code_infor = ["unknown", code_link]
+        code_infor = ["Unknown", code_link]
         return code_infor
 
 
 def github_filter(code_link):
-    # Extract the owner username and name of the warehouse
-    if ".git" in code_link:
-        code_link = code_link.replace(".git", "")
-    owner, repo = code_link.split("/")[-2:]
-    new_code_link = f"https://api.github.com/repos/{owner}/{repo}/languages"
-    # Issue a GET request to obtain the programming language type of the warehouse
-    languages = requests.get(new_code_link)
-    # Check if the response status code is 200 (indicating success)
-    if languages.status_code == 200:
-        # Obtain response JSON data
-        data = languages.json()
-        # Traverse the programming language type and its corresponding number of lines, and print it out
-        for language, lines in data.items():
-            print(f"Deatiled information: {language}, total {lines} lines")
-            code_infors = [language, code_link]
+    # Check if the code_link starts with "http"
+    if code_link.startswith("http"):
+        # Replace ".git" with "" if it is found in the code_link
+        if ".git" in code_link:
+            code_link = code_link.replace(".git", "")
+
+        # Split the code_link to get the owner and repo
+        owner, repo = code_link.split("/")[-2:]
+
+        # Create a new code_link for the API request
+        new_code_link = f"https://api.github.com/repos/{owner}/{repo}/languages"
+
+        try:
+            # Make a request to the API
+            languages = requests.get(new_code_link)
+
+            # Check if the response status code is 200 (indicating success)
+            if languages.status_code == 200:
+                # Obtain response JSON data
+                data = languages.json()
+
+                # Traverse the programming language type and its corresponding number of lines, and print it out
+                for language, lines in data.items():
+                    print(
+                        f"Deatiled information: {language}, total {lines} lines")
+                    # Return the language and code_link
+                    code_infors = [language, code_link]
+                    return code_infors
+            else:
+                print(
+                    f"Request failed with status code {languages.status_code}")
+                # Return the language and code_link
+                code_infors = ["Unknown", code_link]
+                return code_infors
+
+        except requests.exceptions.RequestException as e:
+            # Handle request errors
+            print(f"Request failed with error: {e}")
+            code_infors = ["Error", code_link]
             return code_infors
+
+        except ValueError as e:
+            # Handle JSON decoding errors
+            print(f"JSON decoding failed with error: {e}")
+            code_infors = ["Failed", code_link]
+            return code_infors
+
+        except:
+            # Handle other unknown errors
+            print("An unknown error occurred")
+            code_infors = ["Failed", code_link]
+            return code_infors
+
     else:
-        print(f"Request failed with status code {languages.status_code}")
-        code_infors = ["Unkonw", code_link]
+        print(f"This code_link: {code_link} maybe not a code repository......")
+        code_infors = ["Unknown", code_link]
         return code_infors
-
-
-# def github_filter(code_link):
-#     # Extract the owner username and name of the repository
-#     if ".git" in code_link:
-#         code_link = code_link.replace(".git", "")
-#     owner, repo = code_link.split("/")[-2:]
-#     new_code_link = f"https://api.github.com/repos/{owner}/{repo}/languages"
-
-#     try:
-#         # Issue a GET request to obtain the programming language information of the repository
-#         languages = requests.get(new_code_link)
-#         # Check if the response status code is 200 (indicating success)
-#         if languages.status_code == 200:
-#             # Obtain response JSON data
-#             data = languages.json()
-#             # Traverse the programming language type and its corresponding number of lines, and add it to the code_infos list
-#             for language, lines in data.items():
-#                 print(f"Detailed information: {language}, total {lines} lines")
-#                 code_infors = [language, code_link]
-#             return code_infors
-#         else:
-#             print(f"Request failed with status code {languages.status_code}")
-#             code_infors = ["Unkonw", code_link]
-#             return code_infors
-#     except requests.exceptions.RequestException as e:
-#         # Handle request errors
-#         print(f"Request failed with error: {e}")
-#         return None
-#     except ValueError as e:
-#         # Handle JSON decoding errors
-#         print(f"JSON decoding failed with error: {e}")
-#         return None
-#     except:
-#         # Handle other unknown errors
-#         print("An unknown error occurred")
-#         return None
 
 
 def search_databases(url):
@@ -263,47 +293,64 @@ def search_databases(url):
 
 
 def data_info(data_language, url_data, shields_color_data):
+    """
+    This function takes three parameters: data_language, url_data and shields_color_data
+    """
+
+    # Replace any spaces in the data language with %20
     if " " in data_language:
         data_language = data_language.replace(" ", "%20")
+
+    # Create the shields_url_data using the parameters
     shields_url_data = "https://img.shields.io/badge/-" + \
         data_language + "-" + shields_color_data
 
-    # Merge variables as 'data'
+    # Create the 'Data' string using the parameters
     data = "[" + "!" + "[" + data_language + "]" + \
         "(" + shields_url_data + ")" + "]" + "(" + url_data + ")"
     return data
 
 
 def code_info(code_language, url_code, shields_color_code):
+    """
+    This function takes three parameters: code_language, url_code and shields_color_code
+    """
+
+    # Replace any spaces in the code language with %20
     if " " in code_language:
         code_language = code_language.replace(" ", "%20")
+
+    # Create the code string using the URL and the code language
     shields_url_code = "https://img.shields.io/badge/-" + \
         code_language + "-" + shields_color_code
 
-    # Merge variables as 'Code'
+    # Create the 'Code' string using the parameters
     code = "[" + "!" + "[" + code_language + "]" + \
         "(" + shields_url_code + ")" + "]" + "(" + url_code + ")"
     return code
 
 
 def check_color(x):
-    if x == "R":
-        shields_color = "75aadb"
-    elif x == "Python":
-        shields_color = "3572a5"
-    elif x == "Shell":
-        shields_color = "89e051"
-    elif x == "Jupyter Notebook":
-        shields_color = "da5b0b"
-    elif x == "GEO":
-        shields_color = "336699"
-    elif x == "Zenodo":
-        shields_color = "024dad"
-    elif x == "PKU":
-        shields_color = "357ca5"
-    elif x == "figshare":
-        shields_color = "c62764"
+    """
+    This function takes a string as an argument and returns a hex code based on the value of the string.
+    """
+
+    # Dictionary to store the hex codes for each string
+    color_dict = {
+        "R": "75aadb",
+        "Python": "3572a5",
+        "Shell": "89e051",
+        "Jupyter Notebook": "da5b0b",
+        "GEO": "336699",
+        "Zenodo": "024dad",
+        "PKU": "357ca5",
+        "figshare": "c62764",
+        "Failed": "c02f31"
+    }
+    # Check if the argument is in the dictionary
+    if x in color_dict:
+        shields_color = color_dict[x]
     else:
         x = "Unknown"
-        shields_color = "black"
+        shields_color = "ca1fc7"
     return shields_color
