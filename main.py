a1d959b2d58ae9286678c1d61175d35005257189
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-05-17 19:47:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,55 @@
 '''
 This repository is builded and maintained by Mengxu(mengxu98@qq.com).
 If you interested it, please add items as follow rules, and PR:
-You can choose one of the following ways:
+You can choose one of the following two ways:
 
-1) Refer to the 'README' file and manually fill in the following information:
+1) Automatically fill in information using 'extract_paper_infor' function:
+    Parameters setting:
+        Parameter: 'url_paper', need to provide a link of the paper from https://www.semanticscholar.org/;
+        Parameter: 'doi_paper', need to provide a doi of the paper;
+        Parameter: 'code_language', the main programming languages used of the paper;
+        Parameter: 'url_code', code storage address of the paper;
+        Parameter: 'data_database', database for storing data of the paper;
+        Parameter: 'url_data', data storage address of the paper.
+
+    Note: please shut down the proxy service when using this script!!!
+         
+    Note: now this script only supports some open access journals correctly, such as "Nature communications", 
+        and some journals will not allow extract information.
+        So, you should use semanticscholar APIs as much as possible to obtain information,
+        and manually fill in other parameters, such as 'code_language', 'url_code', 'data_database' and 'url_data'.
+
+2) Refer to the 'README' file and manually fill in the following information:
     Add "| Journal | Date | Title | Code | Data | Citation |";
         For Journal and Title, please sort by A-Z;
         For Date, please sort by published date;
         For Code and Data, please reference: https://img.shields.io/;
         For Citation, the data of Citation could obtain from: https://www.semanticscholar.org/.
 
-2) Automatically fill in information using 'extract_paper_infor' function:
-    Parameter: 'url_paper', need to provide a link to the paper from https://www.semanticscholar.org/;
-    Parameter: 'code_language', the main programming languages used in the paper;
-    Parameter: 'url_code', code storage address in the paper;
-    Parameter: 'data_database', database for storing data in the paper;
-    Parameter: 'url_data', data storage address in the paper.
-
-    Note: please shut down the proxy service when using this script!
-    
-Note: now this script only supports some open access journals correctly, such as "Nature communications", 
-      and some journals will not allow extract information.
-      So, you should use semanticscholar APIs as much as possible to obtain information,
-      and manually fill in other parameters, such as 'code_language', 'url_code', 'data_database' and 'url_data'.
-
 If you encounter any problems when using this script, please issue on GitHub or contact me.
 '''
 
-# Import function
-from extract_paper_information import extract_paper_infor
 
-######################
-# ***--- Here ---*** #
-######################
+# Import function
+from functions.extract_paper_information import extract_paper_infor
+
+##################################################################################################
+# ***----------------------------------------- Here -----------------------------------------*** #
+##################################################################################################
 
 # The URL of paper obtain from: https://www.semanticscholar.org/
 url_paper = "https://www.semanticscholar.org/paper/Single-cell-analysis-reveals-prognostic-fibroblast-Hanley-Waise/a5b158dbafff3ade8a9779c134441e5f3db18e2d"
-url_paper = "https://api.semanticscholar.org/v1/paper/a5b158dbafff3ade8a9779c134441e5f3db18e2d"  # test
+# url_paper = "https://api.semanticscholar.org/v1/paper/a5b158dbafff3ade8a9779c134441e5f3db18e2d"
+# doi_paper = "https://doi.org/10.1038/s41467-023-35832-6"
+doi_paper = ""
 code_language = ""
 url_code = ""
 data_database = ""
 url_data = ""
 
+
 extract_paper_infor(url_paper,
+                    doi_paper,
                     code_language,
                     url_code,
                     data_database,
